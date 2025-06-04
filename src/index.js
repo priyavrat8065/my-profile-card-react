761ec3240173badf1e5de5d233af83c4272c161a
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2025-06-04 15:31:49 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function CardDesc() {
+  return (
+    <main className="card-desc">
+      <h1>Priyavrat Chaudhary</h1>
+      <p>
+        I am a full-stack web developer and a teacher at udemy. When not coding
+        or preparing a course, I like to play board games, to cook(and eat), or
+        to enjoy just Portuguese sun at the beach.
+      </p>
+      <Skills />
+    </main>
+  );
+}
+function Skill(props) {
+  return (
+    <div className={props.className}>
+      {props.skillset} {props.emoji}
+    </div>
+  );
+}
+function Skills() {
+  return (
+    <div className="skills">
+      <Skill skillset="HTML+CSS" emoji="💪🏽" className="skill--1" />
+      <Skill skillset="Javascript" emoji="💪🏽" className="skill--2" />
+      <Skill skillset="Web Design" emoji="👶🏼" className="skill--3" />
+      <Skill skillset="Git & GitHub" emoji="💪🏽" className="skill--4" />
+      <Skill skillset="React" emoji="👶🏼" className="skill--5" />
+      <Skill skillset="Python" emoji="💪🏽" className="skill--6" />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="card">
+      <img src="priyavrat.png" alt="pizza" width="466px" />
+
+      <CardDesc />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
